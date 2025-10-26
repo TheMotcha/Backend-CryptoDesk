9aefae08b4be355260aa2f1a407a9d8759a37e77
TheMoctha <riveraromanjoseandres@gmail.com>
2025-10-26 09:08:42 -0500

    Se actualizan las clases de Service y Controller para poder hacer las operaciones de CRUD, con respecto a los Archivos

--- src/main/java/com/cryptodesk/cryptodesk/Controller/ArchivoController.java
+++ src/main/java/com/cryptodesk/cryptodesk/Controller/ArchivoController.java
@@ -1,7 +1,55 @@
 package com.cryptodesk.cryptodesk.Controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import com.cryptodesk.cryptodesk.Entity.Archivo;
+import com.cryptodesk.cryptodesk.Service.ArchivoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/archivos")
 public class ArchivoController {
+
+        @Autowired
+        private ArchivoService archivoService;
+
+        // 🔹 Crear usuario
+        @PostMapping
+        public ResponseEntity<Archivo> crearUsuario(@RequestBody Archivo archivo) {
+            Archivo nuevoArchivo = archivoService.guardar(archivo);
+            return ResponseEntity.ok(nuevoArchivo);
+        }
+
+        // 🔹 Obtener todos
+        @GetMapping
+        public ResponseEntity<List<Archivo>> listarUsuarios() {
+            List<Archivo> usuarios = archivoService.listarTodos();
+            return ResponseEntity.ok(usuarios);
+        }
+
+        // 🔹 Buscar por ID
+        @GetMapping("/{id}")
+        public ResponseEntity<Archivo> obtenerPorId(@PathVariable int id) {
+            Optional<Archivo> usuario = archivoService.buscarPorId(id);
+            return usuario.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
+
+        // 🔹 Actualizar usuario
+        @PutMapping("/{id}")
+        public ResponseEntity<Archivo> actualizarUsuario(@PathVariable int id, @RequestBody Archivo usuario) {
+            usuario.setIdArchivo(id); // Aseguramos que el ID de la URL prevalezca
+            Archivo actualizado = archivoService.guardar(usuario);
+            return ResponseEntity.ok(actualizado);
+        }
+
+        // 🔹 Eliminar usuario
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> eliminarUsuario(@PathVariable int id) {
+            archivoService.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
 }
--- src/main/java/com/cryptodesk/cryptodesk/Service/ArchivoService.java
+++ src/main/java/com/cryptodesk/cryptodesk/Service/ArchivoService.java
@@ -1,4 +1,4 @@
-package com.cryptodesk.cryptodesk.service;
+package com.cryptodesk.cryptodesk.Service;
 
 import com.cryptodesk.cryptodesk.Entity.Archivo;
 import com.cryptodesk.cryptodesk.Repository.ArchivoRepository;
