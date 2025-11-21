be2316cca60e3f45d13a91dc27d3578078c13c36
TheMoctha <riveraromanjoseandres@gmail.com>
2025-11-21 13:55:31 -0500

    Se comenta jakarta y se usa javax, debido a la version del springboot

--- src/main/java/com/cryptodesk/cryptodesk/Entity/EncryptedFile.java
+++ src/main/java/com/cryptodesk/cryptodesk/Entity/EncryptedFile.java
@@ -1,6 +1,7 @@
-package com.cryptodesk.cryptodesk.Model;
+package com.cryptodesk.cryptodesk.Entity;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
 
